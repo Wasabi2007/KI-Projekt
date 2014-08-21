e9ce248e532ea3a29076fccf99d8d4490e723709
Jerry <Jeremias.Boos@gmx.de>
2014-08-21 10:15:19 +0200

    Some small neat UI things added

---
+++ Assets/Scripts/UIButtonEnable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonEnable : MonoBehaviour {
+
+	public UIButton Target;
+	public bool state = true;
+	
+	void OnClick () { if (Target != null) Target.isEnabled = state; }	
+}
